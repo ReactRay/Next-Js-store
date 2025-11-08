8e0b8e4024dd4a0cfe511c769b6b54a9e3ac9f25
Radwan Mansur <raydiaz1899@gmail.com>
2025-11-08 05:06:14 +0200

    custom form components and buttons

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,20 @@
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+
+const ImageInput = () => {
+
+
+    const name = 'image'
+
+
+    return (
+        <div className='mb-2'>
+            <Label htmlFor={name} className='capitalize'>Image</Label>
+            <Input id={name} type="file" name={name} required
+                accept='image/*' />
+
+        </div>
+    )
+}
+
+export default ImageInput
